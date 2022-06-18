9bd28f89b81c754883c808868203ad5d23062c8f
Zifeng-Jiang <78604382+Zifeng-Jiang@users.noreply.github.com>
2022-06-18 04:10:57 -0400

    Update ship or no ship.py

--- Functions and Pytest/ship or no ship.py
+++ Functions and Pytest/ship or no ship.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
+from skimage.io import imread
 
 def search_ship():
     '''
     Input: type: A string 'ship' or 'noship'
-    Output: One of the images' name with ship(s) or noship in our dataset if input is 'ship' or 'noship', respectively.
+    Output: One of the images' numpy array with ship(s) or noship in our dataset if input is 'ship' or 'noship', respectively.
     '''
     UserInput = input("Enter ship or noship")
     
     if UserInput == "ship":
-        return "000155de5.jpg"
+        return imread('input/train_v2/' + "000155de5.jpg")
     elif UserInput == "noship":
-        return "00003e153.jpg"
+        return imread('input/train_v2/' + "00003e153.jpg")
     else:
         return "Please type in 'ship' or 'noship'."
 
